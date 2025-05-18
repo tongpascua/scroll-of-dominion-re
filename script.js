63df5372a6a601ba4b7ee9285cbdc02ab6c2988e
tongpascua <tongpascua@gmail.com>
2025-05-18 14:24:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("scrollbotBtn").onclick = function () {
+  const xpFill = document.querySelector(".xp-fill");
+
+  function triggerXPAnimation() {
+    xpFill.style.transition = "none";
+    xpFill.style.width = "0%";
+    void xpFill.offsetWidth;
+    xpFill.style.transition = "width 1s ease-in-out";
+    xpFill.style.width = "100%";
+
+    setTimeout(() => {
+      xpFill.style.transition = "width 0.5s ease-in-out";
+      xpFill.style.width = "80%";
+    }, 2000);
+  }
+
+  document.querySelector(".scroll-btn:nth-of-type(1)").onclick = function () {
     alert("ScrollBot summoned!");
+    triggerXPAnimation();
   };
-  document.getElementById("cryptoSealBtn").onclick = function () {
+
+  document.querySelector(".scroll-btn:nth-of-type(2)").onclick = function () {
     alert("Accessing CryptoSeal...");
+    triggerXPAnimation();
   };
-  document.getElementById("xpCircleBtn").onclick = function () {
+
+  document.querySelector(".scroll-btn:nth-of-type(3)").onclick = function () {
     alert("XP Circle Unlocked!");
+    triggerXPAnimation();
   };
 });
