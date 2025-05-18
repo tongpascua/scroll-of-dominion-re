4d2da23dab8b53249f5789b38c09e5ed0ecbbc95
tongpascua <tongpascua@gmail.com>
2025-05-18 13:44:33 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("scrollbotBtn").onclick = function () {
+    alert("ScrollBot summoned!");
+  };
+  document.getElementById("cryptoSealBtn").onclick = function () {
+    alert("Accessing CryptoSeal...");
+  };
+  document.getElementById("xpCircleBtn").onclick = function () {
+    alert("XP Circle Unlocked!");
+  };
+});
